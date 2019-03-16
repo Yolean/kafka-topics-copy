032e5885c8b1515fa37d3cb074ec3f54b5983f6e
Staffan Olsson <staffan@repos.se>
2019-03-16 15:54:15 +0100

    mvn io.quarkus:quarkus-maven-plugin:0.11.0:create \     -DprojectGroupId=se.yolean.kafka \     -DprojectArtifactId=quarkus-kafka \     -DclassName="se.yolean.kafka.quarkus.QuarkusKafkaClient" \     -Dpath="/client"

---
+++ src/test/java/se/yolean/kafka/quarkus/NativeQuarkusKafkaClientIT.java
@@ -0,0 +1,9 @@
+package se.yolean.kafka.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeQuarkusKafkaClientIT extends QuarkusKafkaClientTest {
+
+    // Execute the same tests but in native mode.
+}
---
+++ src/test/java/se/yolean/kafka/quarkus/QuarkusKafkaClientTest.java
@@ -0,0 +1,21 @@
+package se.yolean.kafka.quarkus;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+
+@QuarkusTest
+public class QuarkusKafkaClientTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+          .when().get("/client")
+          .then()
+             .statusCode(200)
+             .body(is("hello"));
+    }
+
+}
