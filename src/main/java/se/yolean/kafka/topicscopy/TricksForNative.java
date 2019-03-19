4ea1befd1e1cb4770c96fbf5b89d2848162adbac
Staffan Olsson <staffan@repos.se>
2019-03-19 21:52:43 +0100

    Shows a trace in log that the class loading took place

--- src/main/java/se/yolean/kafka/topicscopy/TricksForNative.java
+++ src/main/java/se/yolean/kafka/topicscopy/TricksForNative.java
@@ -3,8 +3,12 @@ package se.yolean.kafka.topicscopy;
 import java.io.IOException;
 
 import org.apache.kafka.common.record.CompressionType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public abstract class TricksForNative {
+
+  static final Logger logger = LoggerFactory.getLogger(TricksForNative.class);
 
   /**
    * https://github.com/Yolean/kafka-topics-copy/issues/4
@@ -20,6 +24,7 @@ public abstract class TricksForNative {
       new org.xerial.snappy.SnappyOutputStream(new java.io.ByteArrayOutputStream()).close();
     } catch (IOException e) {
     }
+    logger.info("Tried to enable snappy support");
   }
 
 }
