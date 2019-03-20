baadf48b36aae85a019b4c1b5f228260a216269e
Staffan Olsson <staffan@repos.se>
2019-03-20 17:07:37 +0100

    Temp

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 
 set -e
 
-export GRAALVM_HOME=/opt/graalvm-ce-1.0.0-rc13/Contents/Home/
-export PATH=$GRAALVM_HOME/bin:$PATH:/opt/apache-maven-3.6.0/bin
-export JAVA_HOME=$GRAALVM_HOME
+#export GRAALVM_HOME=/opt/graalvm-ce-1.0.0-rc13/Contents/Home/
+#export PATH=$GRAALVM_HOME/bin:$PATH:/opt/apache-maven-3.6.0/bin
+#export JAVA_HOME=$GRAALVM_HOME
 
 # Verify dev environment with graalvm
 #java -version
@@ -14,13 +14,10 @@ export JAVA_HOME=$GRAALVM_HOME
 # Local use
 #mvn package -Pnative
 
-# x64
-mvn dependency:tree
-mvn test
-
-#mvn package -Pnative -Dnative-image.docker-build=true
+# Local, official quarkus plugin method, native executable image
+#mvn package -Pnative -Dnative-image.docker-build=true -Dmaven.test.skip=true
 #docker build -f src/main/docker/Dockerfile -t yolean/kafka-topics-copy:dev .
-mvn package
+
 docker build -f ./Dockerfile -t yolean/kafka-topics-copy:dev .
 
 build-contract
